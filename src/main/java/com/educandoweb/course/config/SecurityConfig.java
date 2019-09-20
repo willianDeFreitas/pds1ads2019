05a21b8b5e03d2393f1a4b0e95e14b2ffaca5033
Nelio Alves <acenelio@gmail.com>
2019-09-20 13:17:16 -0300

    Authorization filter

--- src/main/java/com/educandoweb/course/config/SecurityConfig.java
+++ src/main/java/com/educandoweb/course/config/SecurityConfig.java
@@ -13,10 +13,14 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.educandoweb.course.security.JWTAuthorizationFilter;
+import com.educandoweb.course.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
     private Environment env;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
@@ -44,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.anyRequest().authenticated();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 	}
 
 	// SWAGGER
